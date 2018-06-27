78f234dd2ba10cd05d09e06da0749f85c2fcecbd
Ashiri <ajaoiyanu@gmail.com>
2018-06-27 21:21:00 +0100

    Add cloudinary imagefield to profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.shortcuts import reverse
 
+from cloudinary.models import CloudinaryField
+
 from accounts.models import Tenant
 
 # Create your models here.
@@ -9,6 +11,7 @@ from accounts.models import Tenant
 class Profile(models.Model):
     user = models.OneToOneField(Tenant, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
+    picture = CloudinaryField('image', blank=True, null=True)
 
     class Meta:
         verbose_name_plural = 'profiles'
