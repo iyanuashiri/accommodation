67ce1188245252e7c2fb497842863551730ee1c9
Ashiri <ajaoiyanu@gmail.com>
2018-06-27 17:47:34 +0100

    Create profile models

---
+++ profiles/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.shortcuts import reverse
+
+from accounts.models import Tenant
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(Tenant, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'profiles'
+
+    def __str__(self):
+        return '{0}\'s profile'.format(self.user.fullname)
+
+    def get_absolute_url(self):
+        return reverse('profiles:detail', kwargs={'pk': self.pk})
+
+    def get_date_of_birth(self):
+        return self.date_of_birth
+
+
