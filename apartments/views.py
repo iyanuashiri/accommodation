0d93229c0ad5a81b0b7f5dcd194ab6b4477eed71
Ashiri <ajaoiyanu@gmail.com>
2018-06-29 00:40:58 +0100

    Create CRUD view functions for apartments

--- apartments/views.py
+++ apartments/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+
+from .models import Apartment
 
 # Create your views here.
+
+
+class ApartmentList(LoginRequiredMixin, SuccessMessageMixin, generic.ListView):
+    model = Apartment
+    template_name = 'apartments/apartment_list.html'
+    context_object_name = 'apartments'
+    paginate_by = 10
+
+
+class ApartmentDetail(LoginRequiredMixin, SuccessMessageMixin, generic.DetailView):
+    model = Apartment
+    template_name = 'apartments/apartment_detail.html'
+    context_object_name = 'apartment'
+
+
+class ApartmentFormMixin(object):
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
+
+
+class ApartmentCreate(LoginRequiredMixin, SuccessMessageMixin, ApartmentFormMixin, generic.CreateView):
+    model = Apartment
+    template_name = 'apartments/apartment_form.html'
+    fields = ('title', 'picture', 'description', 'location', 'rent', 'number_of_rooms', 'house_type', 'duration')
+    success_message = "%(name)s was updated successfully"
+
+
+class ApartmentUpdate(LoginRequiredMixin, SuccessMessageMixin, ApartmentFormMixin, generic.UpdateView):
+    model = Apartment
+    template_name = 'apartments/apartment_form.html'
+    fields = ('title', 'picture', 'description', 'location', 'rent', 'number_of_rooms', 'house_type', 'duration')
+    success_message = "%(name)s was updated successfully"
+
+
+class ApartmentDelete(LoginRequiredMixin, SuccessMessageMixin, generic.UpdateView):
+    model = Apartment
+    template_name = 'apartments/apartment_confirm_delete.html'
+    success_url = reverse_lazy('apartments:delete')
+    success_message = "%(name)s was updated successfully"
+
