fd7e8b25bd48e3c00431cac94fb8bba5835bcacf
Ashiri <ajaoiyanu@gmail.com>
2018-06-30 17:48:17 +0100

    Add support for is_staff User

--- accounts/models.py
+++ accounts/models.py
@@ -24,6 +24,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(_('first name'), max_length=100)
     last_name = models.CharField(_('last name'), max_length=100)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
     is_active = models.BooleanField(_('active'), default=True)
 
     objects = UserManager()
