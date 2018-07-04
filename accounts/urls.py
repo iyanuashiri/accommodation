e7fe2aec759861a361cacfa55096f56e146dac9d
Ashiri <ajaoiyanu@gmail.com>
2018-07-04 13:45:13 +0100

    FIxes

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('tenant/signup/', views.TenantSignUpView.as_view(), name='tenant_signup'),
-    path('landlord/signup/', views.LandLordSignUpView.as_view(), name='landlord_signup')
+    path('landlord/signup/', views.LandLordSignUpView.as_view(), name='landlord_signup'),
 ]
