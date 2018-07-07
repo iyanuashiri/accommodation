06555d58c8833e89167ac7d7cc79d24ebb318975
Ashiri <ajaoiyanu@gmail.com>
2018-07-07 03:05:59 +0100

    Add slideshow to homepage

--- apartments/static/javascript/main.js
+++ apartments/static/javascript/main.js
@@ -1,3 +1,17 @@
 $('.ui.dropdown')
   .dropdown()
 ;
+
+
+
+$(document).ready(function(){
+  $('.your-class').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 1500
+  });
+});
